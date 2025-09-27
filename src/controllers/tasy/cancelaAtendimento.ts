b54f7ea931e809b412095a9695a394a093ae7c93
root <root@PC3-TI-SD.cerdil.com.br>
2025-09-27 10:28:43 -0400

    Controller refatorada e testada

--- src/controllers/tasy/cancelaAtendimento.ts
+++ src/controllers/tasy/cancelaAtendimento.ts
@@ -1,13 +1,13 @@
 import { HttpRequest, HttpResponse } from '../../interfaces';
-import { getConnectionPacs, getConnectionTasy } from '../../database';
-import { cancelaAtendimento, selectContaPaciente, selectStatusAtendimento, selectNrPrescricao, selectSequenciaInterna, selectAcessionNumber, selectCodigoUsuario} from './query';
-import OracleDB from 'oracledb';
+import { getConnectionPacs, getConnectionTasyProd } from '../../database';
+import { TasyService } from '../../services/tasy/tasy-service';
+import { PacsService } from '../../services/pacs/pacs-service';
 class CancelaAtendimentoController {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
 
             const { atendimentoId, motivoCancelamento, nomeUsuario } = httpRequest.body
-            
+
             if (!atendimentoId) {
                 return {
                     statusCode: 400,
@@ -15,16 +15,16 @@ class CancelaAtendimentoController {
                 };
             }
 
-            const atendimentoIdNumber = Number(atendimentoId);
-            
-            if (isNaN(atendimentoIdNumber) || !Number.isInteger(atendimentoIdNumber)) {
+            const idAtendimento = Number(atendimentoId);
+
+            if (isNaN(idAtendimento) || !Number.isInteger(idAtendimento)) {
                 return {
                     statusCode: 400,
                     body: { error: "Atendimento ID inválido" },
                 };
             }
 
-            if (atendimentoIdNumber <= 0 || atendimentoId.length > 7){
+            if (idAtendimento <= 0 || atendimentoId.length > 7) {
                 return {
                     statusCode: 400,
                     body: { error: "Tamanho do Atendimento ID invalido " },
@@ -51,35 +51,75 @@ class CancelaAtendimentoController {
                     body: { error: "Motivo de cancelamento muito longo" },
                 };
             }
-            
-            const connectionTasy = await getConnectionTasy();
 
-            const statusAtendimento = await connectionTasy.execute(
-                selectStatusAtendimento,
-                { atendimentoIdNumber },
-            ) // feito
+            if (!nomeUsuario) {
+                return {
+                    statusCode: 400,
+                    body: { error: "É necessário informar o nome do usuário que está realizando o cancelamento" },
+                };
+            }
 
-            if (statusAtendimento.rows && statusAtendimento.rows.length > 0) {
-                const row = statusAtendimento.rows[0] as [any, any];
-                const dtCancelamento = row[0];
-                const usuarioCancelamento = row[1];
+            if (nomeUsuario.length < 3 || nomeUsuario.length > 30) {
+                return {
+                    statusCode: 400,
+                    body: { error: "Nome do usuário inválido" },
+                };
+            }
 
-                if (dtCancelamento !== null || usuarioCancelamento !== null) {
-                    await connectionTasy.close();
+            const tasyService = new TasyService(await getConnectionTasyProd());
+
+            const statusAtendimento = await tasyService.AtendimentoCancelado(idAtendimento);
+
+            if (statusAtendimento) {
+                const { dataCancelamento, usuarioCancelamento } = statusAtendimento;
+
+                if (dataCancelamento !== null || usuarioCancelamento !== null) {
+                    await tasyService.closeConnection();
                     return {
                         statusCode: 400,
-                        body: { error:`Atendimento já está cancelado`, data : { dtCancelamento, usuarioCancelamento } },
+                        body: { error: `Atendimento já está cancelado`, data: { dataCancelamento, usuarioCancelamento } },
                     };
                 }
             }
 
-            const contaPaciente = await connectionTasy.execute(
-                selectContaPaciente,
-                { atendimentoIdNumber },
-            ); // feito
+            const nrPrescricao = await tasyService.numeroPrescricao(idAtendimento);
+            console.log('nrPrescricao:', nrPrescricao);
 
-            if (contaPaciente.rows && contaPaciente.rows.length === 0) {
-                await connectionTasy.close();
+
+            if (!nrPrescricao || nrPrescricao <= 0) {
+                await tasyService.closeConnection();
+                return {
+                    statusCode: 404,
+                    body: { error: "Atendimento não tem prescrição médica, logo o cancelamento deve ser manual" },
+                };
+            }
+
+
+
+            const nrSequenciaInterna = await tasyService.nrSequenciaInterna(nrPrescricao);
+            console.log('nrSequenciaInterna:', nrSequenciaInterna);
+
+            // Logica de busco no PACS
+
+            const pacsService = new PacsService(await getConnectionPacs())
+            const resultPacsSelect = await pacsService.consultaSequenciaInterna(nrSequenciaInterna)
+
+
+            if (resultPacsSelect) {
+                pacsService.closeConnection()
+                return {
+                    statusCode: 400,
+                    body: { error: "Atendimento tem exame no PACS" },
+                };
+            }
+
+
+
+            const contaPaciente = await tasyService.contaPaciente(idAtendimento);
+
+
+            if (contaPaciente === false) {
+                tasyService.closeConnection();
                 return {
                     statusCode: 404,
                     body: { error: "Atendimento não tem conta paciente, logo o cancelamento deve ser manual" },
@@ -87,117 +127,22 @@ class CancelaAtendimentoController {
             }
 
 
-            const resultSelectPrescricao = await connectionTasy.execute(
-                selectNrPrescricao,
-                { atendimentoIdNumber },
-                { outFormat: OracleDB.OUT_FORMAT_OBJECT }
-
-            ); // feito
-
-            // console.log('nrPrescricao:', resultSelectPrescricao.rows[0]);
-
-            // Pegar o nuerode sequencia interna
-            const row = resultSelectPrescricao.rows[0] as { NR_PRESCRICAO: number };
-            var nrPrescricao = row?.NR_PRESCRICAO;
-
-            if (!resultSelectPrescricao.rows.length ) {
-                await connectionTasy.close();
-                return {
-                    statusCode: 404,
-                    body: { error: "Atendimento não tem prescrição médica" },
-                };
-                
-            }
-
-
-            if (!nrPrescricao || nrPrescricao <= 0) {
-                await connectionTasy.close();
-                return {
-                    statusCode: 404,
-                    body: { error: "Atendimento não tem prescrição médica, logo o cancelamento deve ser manual" },
-                };
-            }
-            
-
-            
-
-            const nrSequenciaInterna = await connectionTasy.execute(
-                selectSequenciaInterna,
-                { nrPrescricao },
-            ); // feito
-            
-            // console.log('nrSequenciaInterna:', nrSequenciaInterna.rows[0]);
-
-            const rowSeq = nrSequenciaInterna.rows[0] as [string];
-            var nrSeqInterna = rowSeq[0].toString();
-            
-            // Logica de busco no PACS
-
-            // const connectionPacs = await getConnectionPacs()
-            // const resultSelectAcessionNumber = await connectionPacs.execute(
-            //     selectAcessionNumber,
-            //     { accessionNumber: nrSeqInterna},
-            //     // { outFormat: OracleDB.OUT_FORMAT_OBJECT }
-            // );
-
-            // console.log(`Resultado select ${resultSelectAcessionNumber}`)
-
-            // if (resultSelectAcessionNumber.rows && resultSelectAcessionNumber.rows.length > 0) {
-            //     await connectionTasy.close();
-            //     return {
-            //         statusCode: 400,
-            //         body: { error: "Atendimento tem exame no PACS" },
-            //     };
-            // }
-            
-            if (!nomeUsuario) {
-                await connectionTasy.close();
-                return {
-                    statusCode: 400,
-                    body: { error: "É necessário informar o nome do usuário que está realizando o cancelamento" },
-                };
-            }
-            if (nomeUsuario.length < 3 || nomeUsuario.length > 30) {
-                await connectionTasy.close();
-                return {
-                    statusCode: 400,
-                    body: { error: "Nome do usuário inválido" },
-                };
-            }
-
             // Busca codigo do usuario 
-            const rowCodUsuario = await connectionTasy.execute(
-                selectCodigoUsuario,
-                { nomeUsuario }, // feito
-            );
+            console.log('nomeUsuario:', nomeUsuario);
+            const codUsuario = await tasyService.codigoUsuario(nomeUsuario);
 
-            if (rowCodUsuario.rows && rowCodUsuario.rows.length === 0) {
-                await connectionTasy.close();
+            if (!codUsuario) {
+                await tasyService.closeConnection();
                 return {
                     statusCode: 404,
                     body: { error: "Usuário não encontrado" },
                 };
             }
 
-            //
-            const codigoUsuario = rowCodUsuario.rows[0] as [number];
-            console.log('codigoUsuario:', codigoUsuario);
-            console.log('codigoUsuario:', codigoUsuario[0]);
-
-            const result = await connectionTasy.execute(
-                cancelaAtendimento,
-                {
-                    atendimentoId,
-                    nomeUsuario,
-                    codigoUsuario : codigoUsuario[0],
-                    motivoCancelamento,
-                },
-            );
-
-            connectionTasy.commit();
+            const result = await tasyService.cancelarAtendimento(atendimentoId, nomeUsuario, codUsuario, motivoCancelamento);
 
             if (!result) {
-                await connectionTasy.close();
+                await tasyService.closeConnection();
                 return {
                     statusCode: 500,
                     body: { error: "Erro ao cancelar o atendimento" },
@@ -205,7 +150,8 @@ class CancelaAtendimentoController {
             }
 
 
-            await connectionTasy.close();
+            await tasyService.closeConnection();
+
 
             return {
                 statusCode: 200,

c28fdbf43e3ae02185d0f6cd7a1aa994f811967b
kaio-d-silva <kaio.contacomercial@gmail.com>
2025-09-27 11:10:20 -0400

    configuração env

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,10 +5,17 @@ dotenv.config();
 
 // Exporta as variáveis de ambiente para uso em todo o projeto
 export const ENV = {
-  PORT: process.env.PORT || '3000',
-  DATABASE_URL: process.env.DATABASE_URL || '',
-  JWT_SECRET: process.env.JWT_SECRET || '',
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1h',
-  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || '',
-  JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+PORT: process.env.PORT || '3000',
+
+USER_DB_TASY_PROD :process.env.USER_DB_TASY_PROD,
+PASSWORD_DB_TASY_PROD :process.env.PASSWORD_DB_TASY_PROD,
+CONNECT_STRING_TASY_PROD :process.env.CONNECT_STRING_TASY_PROD,
+ 
+USER_DB_TASY_HOMO :process.env.USER_DB_TASY_HOMO,
+PASSWORD_DB_TASY_HOMO :process.env.PASSWORD_DB_TASY_HOMO,
+CONNECT_STRING_TASY_HOMO :process.env.CONNECT_STRING_TASY_HOMO,
+ 
+USER_DB_PACS :process.env.USER_DB_PACS,
+PASSWORD_DB_PACS :process.env.PASSWORD_DB_PACS,
+CONNECT_STRING_PACS :process.env.CONNECT_STRING_PACS,
 };
